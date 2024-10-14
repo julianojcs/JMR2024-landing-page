3b63b1980d2c92f35693854c1891b95764fe0fe1
Juliano Costa Silva <apfjuliano@gmail.com>
2024-10-14 02:57:30 -0300

    Excluding /middleware.js

--- middleware.js
+++ middleware.js
@@ -2,5 +2,5 @@ export { default } from 'next-auth/middleware'
 
 // create an array of paths that you want to protect
 export const config = {
-  matcher: ['/medicos/add', '/medicos/saved', '/profile', '/messages']
+  matcher: ['']
 }
