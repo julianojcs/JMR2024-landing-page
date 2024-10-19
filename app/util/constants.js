4670b519847fab89346b856bc81a41e4d9502609
Juliano Costa Silva <apfjuliano@gmail.com>
2024-10-19 03:16:43 -0300

    Creating a JS Objects Exporting

---
+++ app/util/constants.js
@@ -0,0 +1,163 @@
+import { title } from "process";
+
+export const sponsorsLogo = {
+  promoters: {
+    title: 'Realização:',
+    className: 'promoters',
+    brands: [ {
+        name: 'ABRAMEDE/MG',
+        src: '/logo/abramed-mg.png',
+        href: 'https://www.abramedemg.org.br/',
+        width: 210,
+        height: 80,
+        className: 'abramede',
+      }, {
+        name: 'SRMG',
+        src: '/logo/srmg.png',
+        href: 'http://www.srmg.org.br/',
+        width: 150,
+        height: 80,
+        className: 'srmg',
+      },
+    ]
+  },
+  sponsors: {
+    title: 'Patrocinadores:',
+    className: 'sponsors',
+    level: {
+      gold: {
+        title: 'Ouro',
+        className: 'gold',
+        brands: [
+         {
+            name: 'Bayer',
+            src: '/logo/gold/bayer.png',
+            href: 'https://www.bayer.com.br/pt/',
+            width: 80,
+            height: 80,
+            className: 'bayerClass',
+          }, {
+            name: 'Grupo Fleury',
+            src: '/logo/gold/fleury.png',
+            href: 'https://www.fleury.com.br/',
+            width: 213.33,
+            height: 60,
+            className: 'fleuryClass',
+          }, {
+            name: 'Hermes Pardini',
+            src: '/logo/gold/hermes-pardini.png',
+            href: 'https://www.hermespardini.com.br/',
+            width: 153.3,
+            height: 70,
+            className: null,
+          },
+        ],
+      },
+      silver: {
+        title: 'Prata',
+        className: 'silver',
+        brands: [
+          {
+            name: 'Dasa',
+            src: '/logo/silver/dasa.png',
+            href: 'https://www.dasa.com.br/',
+            width: 53.57,
+            height: 30,
+            className: 'dasaClass',
+          }, {
+            name: 'São Marcos',
+            src: '/logo/silver/sao-marcos.png',
+            href: 'https://saomarcoslaboratorio.com.br/',
+            width: 82.2,
+            height: 70,
+            className: 'saoMarcosClass',
+          }, {
+            name: 'Unimed BH',
+            src: '/logo/silver/unimed-bh.png',
+            href: 'https://www.unimedbh.com.br/',
+            width: 136.17,
+            height: 50,
+            className: null,
+          },
+        ],
+      },
+      bronze: {
+        title: 'Bronze',
+        className: 'bronze',
+        brands: [
+            {
+              name: 'Bracco',
+              src: '/logo/bronze/bracco.png',
+              href: 'https://www.bracco.com.br/pt-br/',
+              width: 91,
+              height: 80,
+              className: 'braccoClass',
+            }, {
+              name: 'Canon Medical Systems',
+              src: '/logo/bronze/canon-medical-systems.png',
+              href: 'https://br.medical.canon/',
+              width: 159.17,
+              height: 60,
+              className: 'canonClass',
+            }, {
+              name: 'Ceu Diagnósticos',
+              src: '/logo/bronze/ceu-diagnosticos.png',
+              href: 'https://www.clinicaceu.com.br/',
+              width: 111.3,
+              height: 60,
+              className: 'ceuClass',
+            }, {
+              name: 'Mhédica',
+              src: '/logo/bronze/mhedica.png',
+              href: 'https://www.mhedica.com.br/',
+              width: 229.23,
+              height: 60,
+              className: 'mhedicaClass',
+            }, {
+              name: 'Supri Medical',
+              src: '/logo/bronze/supri-medical.png',
+              href: 'https://www.supri-medical.com/',
+              width: 229.23,
+              height: 60,
+              className: 'supriMedicalClass',
+            },
+        ],
+      },
+    },
+  },
+  supports: {
+    title: 'Apoio:',
+    className: 'promoters',
+    brands: [
+    {
+      name: 'AMMG',
+      src: '/logo/apoio/ammg.png',
+      href: 'https://ammg.org.br/',
+      width: 168.79,
+      height: 60,
+      className: null,
+    }, {
+        name: 'CBR',
+        src: '/logo/apoio/cbr.png',
+        href: 'https://cbr.org.br/',
+        width: 150,
+        height: 60,
+        className: null,
+      },
+    ],
+  },
+  organizer: {
+    title: 'Organização:',
+    className: 'promoters',
+    brands: [
+      {
+        name: 'Core',
+        src: '/logo/organizacao/core.png',
+        href: '#',
+        width: 150,
+        height: 60,
+        className: null,
+      }
+    ],
+  },
+}
