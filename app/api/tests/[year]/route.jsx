f1389a48be95e8d4777bca13fbecca33789ae56e
Juliano Costa Silva <apfjuliano@gmail.com>
2025-02-23 10:10:18 -0300

    Create test API connectToDatabase

--- app/api/tests/[year]/route.jsx
+++ app/api/tests/[year]/route.jsx
@@ -8,15 +8,17 @@ export const GET = async (request, { params }) => {
 
     await connectToDatabase()
 
-    const speakers = await Speaker.find({ year }).sort({ name: 1 })
-    console.log('speakers', speakers)
+    // const speakers = await Speaker.find({ year }).sort({ name: 1 })
+    // console.log('speakers', speakers)
 
-    return new Response(JSON.stringify(speakers), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+    // return new Response(JSON.stringify(speakers), {
+    return new Response(
+      { message: 'Hello from the Test API' },
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json', 'Cache-Control': 'public, max-age=60, s-maxage=60' },
+      }
+    )
   } catch (error) {
     console.error('API Error:', error);
     return new Response(JSON.stringify({ message: error.message }), {
