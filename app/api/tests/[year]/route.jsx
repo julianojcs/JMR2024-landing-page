ea63ea908b85afc0cdced39befb3226414840a51
Juliano Costa Silva <apfjuliano@gmail.com>
2025-02-24 00:21:00 -0300

    Refactoring code to use Vercel Neon/Prisma

--- app/api/tests/[year]/route.jsx
+++ app/api/tests/[year]/route.jsx
@@ -1,28 +1,34 @@
+import { NextResponse } from 'next/server'
 import { connectToDatabase } from '@/app/lib/database'
-import Speaker from '@/models/Speaker'
 
 export const GET = async (request, { params }) => {
   try {
     const { year } = params
-    console.log('year', year)
 
     await connectToDatabase()
 
-    // const speakers = await Speaker.find({ year }).sort({ name: 1 })
-    // console.log('speakers', speakers)
-
-    // return new Response(JSON.stringify(speakers), {
-    return new Response(
-      { message: 'Hello from the Test API' },
+    return NextResponse.json(
+      { 
+        success: true,
+        message: 'Hello from the Test API',
+        year: year 
+      },
       {
         status: 200,
-        headers: { 'Content-Type': 'application/json', 'Cache-Control': 'public, max-age=60, s-maxage=60' },
+        headers: { 
+          'Content-Type': 'application/json',
+          'Cache-Control': 'public, max-age=60, s-maxage=60'
+        },
       }
     )
   } catch (error) {
-    console.error('API Error:', error);
-    return new Response(JSON.stringify({ message: error.message }), {
-      status: 500
-    })
+    console.error('API Error:', error)
+    return NextResponse.json(
+      { 
+        success: false,
+        message: error.message 
+      },
+      { status: 500 }
+    )
   }
 }
