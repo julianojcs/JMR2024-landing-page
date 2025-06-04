71e32747b36ecee1350f7ce1f536939ba1586ee2
Juliano Costa Silva <apfjuliano@gmail.com>
2025-06-03 21:36:56 -0300

    Fix 2 definition of `verifyDate`

--- app/utils/index.js
+++ app/utils/index.js
@@ -54,22 +54,22 @@ export const validateCPF = (cpf) => {
 //   return value.slice(0, 14);
 // }
 
-export function verifyDate(ddmmaaaa) {
-  // Extrair o dia, mês e ano da string DDMMAAAA
-  const dia = parseInt(ddmmaaaa.slice(0, 2), 10)
-  const mes = parseInt(ddmmaaaa.slice(2, 4), 10) - 1 // Meses em JavaScript começam do 0
-  const ano = parseInt(ddmmaaaa.slice(4), 10)
-
-  // Criar um objeto Date com a data informada
-  const dataInformada = new Date(ano, mes, dia)
-
-  // Obter a data de hoje, sem horas, minutos e segundos
-  const hoje = new Date()
-  hoje.setHours(0, 0, 0, 0)
-
-  // Comparar a data informada com a data de hoje
-  return dataInformada >= hoje
-}
+// export function verifyDate(ddmmaaaa) {
+//   // Extrair o dia, mês e ano da string DDMMAAAA
+//   const dia = parseInt(ddmmaaaa.slice(0, 2), 10)
+//   const mes = parseInt(ddmmaaaa.slice(2, 4), 10) - 1 // Meses em JavaScript começam do 0
+//   const ano = parseInt(ddmmaaaa.slice(4), 10)
+//
+//   // Criar um objeto Date com a data informada
+//   const dataInformada = new Date(ano, mes, dia)
+//
+//   // Obter a data de hoje, sem horas, minutos e segundos
+//   const hoje = new Date()
+//   hoje.setHours(0, 0, 0, 0)
+//
+//   // Comparar a data informada com a data de hoje
+//   return dataInformada >= hoje
+// }
 
 export function verifyDate(dateString, currentYear = new Date().getFullYear()) {
   // Adicionar logs para debug
